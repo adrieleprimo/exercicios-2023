f900b592bd021a8ebbbd02c3b6e91e959077e62a
Adriele Primo <adriele.dejprimo@gmail.com>
2023-10-15 16:52:03 -0300

    Criação do componente resumo

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AnaliseSensorialComponent } from './analise-sensorial/analise-sensorial.component';
+import { ResumoComponent } from './resumo/resumo.component';
 
 
 
@@ -12,6 +13,7 @@ import { AnaliseSensorialComponent } from './analise-sensorial/analise-sensorial
     AppComponent,
     HeaderComponent,
     AnaliseSensorialComponent,
+    ResumoComponent,
   ],
   imports: [
     BrowserModule
